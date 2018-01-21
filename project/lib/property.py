2a6d62c3bbcf4d04e3f80e8133429cd42f1ab761
daiCorrea <daiane.ocorrea@gmail.com>
2018-01-21 18:16:14 -0200

    Adding other params to property

--- project/lib/property.py
+++ project/lib/property.py
@@ -1,8 +1,19 @@
 class Property():
     """This Class provides a way to store properties - hotels, apartments, hostels, ... - informations."""
 
-    def __init__(self, property_name):
+    def __init__(self, property_name, property_type, local, star_rating, week_price, weekend_price,
+                 loyalty_week_price, loyalty_weekend_price):
         self.property_name = property_name
+        self.property_type = property_type
+        self.local = local
+        self.star_rating = star_rating
+        self.week_price = week_price
+        self.weekend_price = weekend_price
+        self.loyalty_week_price = loyalty_week_price
+        self.loyalty_weekend_price = loyalty_weekend_price
 
     def get_info(self):
         return self.property_name
+
+    def verify_price(self, date):
+        print(date)
