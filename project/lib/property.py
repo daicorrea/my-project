a6cd5cb5e5b6eaafb9759256aa8f49ea76e451c2
daiCorrea <daiane.ocorrea@gmail.com>
2018-01-21 22:37:18 -0200

    Adding utils properties

--- project/lib/property.py
+++ project/lib/property.py
@@ -1,6 +1,7 @@
 class Property():
     """This Class provides a way to store properties - hotels, apartments, hostels, ... - informations."""
 
+    #Creating Constructor
     def __init__(self, property_name, property_type, local, star_rating, week_price, weekend_price,
                  loyalty_week_price, loyalty_weekend_price):
         self.property_name = property_name
@@ -12,8 +13,28 @@ class Property():
         self.loyalty_week_price = loyalty_week_price
         self.loyalty_weekend_price = loyalty_weekend_price
 
-    def get_info(self):
+    #Funtions to return information
+    def get_property_name(self):
         return self.property_name
 
-    def verify_price(self, date):
-        print(date)
+    def get_property_type(self):
+        return self.property_type
+
+    def get_property_local(self):
+        return self.property_local
+
+    def get_property_star_rating(self):
+        return self.star_rating
+
+    def get_week_price(self):
+        return self.week_price
+
+    def get_weekend_price(self):
+        return self.weekend_price
+
+    def get_loyalty_week_price(self):
+        return self.loyalty_week_price
+
+    def get_loyalty_weekend_price(self):
+        return self.loyalty_weekend_price
+
--- project/lib/utils.py
+++ project/lib/utils.py
@@ -1,13 +1,17 @@
+# Function to format user input leaving client_type in the beginning of the list and the dates at the end.
 def format_user_input(user_input):
+    # Divide to know the client_type
     user_input = user_input.split(':')
-    user_type = user_input.pop(0)
+    client_type = user_input.pop(0)
+    # Divide to know the dates
     user_input = user_input[0].split(',')
-    user_input.insert(0, user_type)
-    print(user_input)
+    # Join information in only one list to return
+    user_input.insert(0, client_type)
+    return user_input
 
-#Function to verify if param day is week day or weekend
+# Function to verify if param day is week day or weekend
 def verify_day(user_date):
-    day = user_date[user_date.find("(") + 1:user_date.find(")")] #Get what's in the parentheses
+    day = user_date[user_date.find("(") + 1:user_date.find(")")]  # Gets what's inside the parentheses
     if day.lower() in ['mon', 'tues', 'wed', 'thur', 'fri']:
         return 'week'
     elif day.lower() in ['sat', 'sun']:
@@ -15,8 +19,41 @@ def verify_day(user_date):
     else:
         return 'error'
 
-def verify_user_input(user_input):
+# Function to see if the value inputed by the user has a valid format
+def validate_user_input(user_input):
     if (':' and ',') in user_input:
         return True
     else:
         return False
+
+# Function to verify if the client type is Regular or Rewards
+def validate_client_type(client_type):
+    if ((client_type == 'regular') or (client_type == 'rewards')):
+        return True
+    else:
+        return False
+
+# ARRUMAR A BADERNA AQUI
+def get_cheapest_property(quote_list):
+    # Use first values as base to compare
+    base_property = quote_list.pop(0)
+    cheapest = base_property.get_property()
+    min_price = base_property.get_final_price()
+
+    print(base_property.get_property().get_property_name(), base_property.get_final_price())
+    for property_quoted in quote_list:
+        print(property_quoted.get_property().get_property_name(), property_quoted.get_final_price())
+        # Gets minimum price
+        if property_quoted.get_final_price() < min_price:
+            min_price = property_quoted.get_final_price()
+            cheapest = property_quoted.get_property()
+        # If price is the same, compare by star ratings
+        if property_quoted.get_final_price() == min_price:
+            if (property_quoted.get_property().get_property_star_rating()) > (cheapest.get_property_star_rating()):
+                min_price = property_quoted.get_final_price()
+                cheapest = property_quoted.get_property()
+
+    # RETORNAR ISSAQUE E IMPRIMIR NA MAIN
+    print('min ', min_price)
+    print('cheapest ', cheapest.get_property_name())
+
