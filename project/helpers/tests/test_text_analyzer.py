69b51b58cd63335e255688d798e0a9452693dd56
daiCorrea <daiane.ocorrea@gmail.com>
2018-01-24 16:46:57 -0200

    Adding text analyzer test

---
+++ project/helpers/tests/test_text_analyzer.py
@@ -0,0 +1,41 @@
+import unittest
+import os
+
+from .. import text_analyzer
+
+
+class LoadDataTest(unittest.TestCase):
+    """Tests for the ``text_analyzer()`` function."""
+
+    def setUp(self):
+        """Fixure trat creates a file to be used in this test"""
+        self.text_first_right_example = 'Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)'
+        self.text_wrong_example = 'Rewards, 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)'
+
+
+    def test_split_right_text(self):
+        """See if the text format passes for a text with both colon and comma"""
+        expected_splitted_text = ['Rewards', '26Mar2009(thur)', '27Mar2009(fri)', '28Mar2009(sat)']
+        splitted_text = text_analyzer.split_text('Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)')
+        self.assertListEqual(expected_splitted_text, splitted_text)
+
+    def test_split_text_without_comma_and_colon(self):
+        """See if the text format passes for a text without colon and comma in only one string"""
+        expected_splitted_text = ['Rewards26Mar2009(thur)27Mar2009(fri)28Mar2009(sat)']
+        splitted_text = text_analyzer.split_text('Rewards 26Mar2009(thur) 27Mar2009(fri) 28Mar2009(sat)')
+        self.assertListEqual(expected_splitted_text, splitted_text)
+
+    def test_split_text_without_comma(self):
+        """See if the text format passes for a text without colon and comma in only one string"""
+        expected_splitted_text = ['Rewards', '26Mar2009(thur)27Mar2009(fri)28Mar2009(sat)']
+        splitted_text = text_analyzer.split_text('Rewards: 26Mar2009(thur) 27Mar2009(fri) 28Mar2009(sat)')
+        self.assertListEqual(expected_splitted_text, splitted_text)
+
+    def test_split_text_without_colon(self):
+        """See if the text format passes for a text without colon and comma in only one string"""
+        expected_splitted_text = ['Rewards26Mar2009(thur)', '27Mar2009(fri)', '28Mar2009(sat)']
+        splitted_text = text_analyzer.split_text('Rewards26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)')
+        self.assertListEqual(expected_splitted_text, splitted_text)
+
+if __name__ == '__main__':
+    unittest.main()
