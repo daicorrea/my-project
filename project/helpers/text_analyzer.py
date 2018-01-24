bc8a2802ef4b5a4ea774231a361dfb83c0702935
daiCorrea <daiane.ocorrea@gmail.com>
2018-01-24 16:26:30 -0200

    Removing whitespace from input data

--- project/helpers/text_analyzer.py
+++ project/helpers/text_analyzer.py
@@ -3,5 +3,5 @@ import re
 
 # Function to receive a text with colon and comma and return a list with the text split.
 def split_text(text_to_split):
-    text_to_split = re.split('[:,]', text_to_split)
+    text_to_split = re.split('[:,]', text_to_split.replace(' ', ''))  # Removing whitespace from input string
     return text_to_split
